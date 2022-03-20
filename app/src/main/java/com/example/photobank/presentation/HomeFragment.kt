536631c13f07ea71c4485671b5560d11fb117153
tonlirise <tonlirise@gmail.com>
2022-03-20 14:42:18 +0700

    Подключение firebase

--- app/src/main/java/com/example/photobank/presentation/HomeFragment.kt
+++ app/src/main/java/com/example/photobank/presentation/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.example.photobank.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.photobank.R
+import com.google.firebase.auth.FirebaseAuth
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,6 +38,24 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword("tonkladko@ya.ru", "rebustubus")
+            .addOnCompleteListener{
+                if(it.isSuccessful){
+                    Log.d("AppTest", "sigIN succsses")
+                }
+                else{
+                    Log.d("AppTest", "sigIN failur")
+                }
+            }
+
+
+
     }
 
     companion object {
